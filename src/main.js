d02eab402bf65970022000369770acfbe44be947
Andrii-Smolianinov <andr380v@gmail.com>
2023-05-13 00:04:02 +0300

    convert mp3 to text

--- src/main.js
+++ src/main.js
@@ -1,33 +1,51 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { Telegraf } from "telegraf";
+import { Telegraf, session } from "telegraf";
 import { message } from "telegraf/filters";
+import { code } from "telegraf/format";
 import { ogg } from "./ogg.js";
 import { openai } from "./openai.js";
+const INITIAL_SESSION = {
+  messages: [],
+};
 
 const { TELEGRAM_TOKEN } = process.env;
 const bot = new Telegraf(TELEGRAM_TOKEN);
 
+bot.use(session());
+
+bot.command("new", async (context) => {
+  context.session = INITIAL_SESSION;
+  await context.reply("Чекаю на Ваше голосове, або текстове повідомлення");
+});
+
+bot.command("start", async (context) => {
+  context.session = INITIAL_SESSION;
+  await context.reply("Чекаю на Ваше голосове, або текстове повідомлення");
+});
+
 bot.on(message("voice"), async (context) => {
+  context.session ??= INITIAL_SESSION;
   try {
+    await context.reply(
+      code("Обробляється голосове повідомлення. Зачекайте...")
+    );
     const link = await context.telegram.getFileLink(
       context.message.voice.file_id
     );
     const userId = String(context.message.from.id);
     const oggPath = await ogg.create(link.href, userId);
     const mp3Path = await ogg.toMp3(oggPath, userId);
-
     const text = await openai.transcription(mp3Path);
-    const response = await openai.chat(text);
+    await context.reply(code(`Ваш запит: - "${text}"`));
 
-    await context.reply(mp3Path);
+    context.session.messages.push({ role: openai.roles.USER, content: "text" });
+    const response = await openai.chat(messages);
+
+    await context.reply(response.content);
   } catch (error) {
     console.log("Error voice message", error.message);
   }
-});
-
-bot.command("start", async (context) => {
-  await context.reply(JSON.stringify(context.message, null, 2));
 });
 
 bot.launch();
--- src/openai.js
+++ src/openai.js
@@ -1,19 +1,48 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { Configuration, OpenAIApi } from "openai";
+import { createReadStream } from "fs";
 
 const { OPENAI_API_KEY } = process.env;
 
 class OpenAI {
+  roles = {
+    ASSISTANT: "assistant",
+    USER: "user",
+    SYSTEM: "system",
+  };
+
   constructor() {
     const configuration = new Configuration({
       apiKey: OPENAI_API_KEY,
     });
+
     this.openai = new OpenAIApi(configuration);
   }
 
-  chat() {}
-  transcription() {}
+  async chat(messages) {
+    try {
+      const response = await this.openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages,
+      });
+      return response.data.choices[0].message
+    } catch (error) {
+      console.log("Error GPT-chat", error.message);
+    }
+  }
+
+  async transcription(filepath) {
+    try {
+      const response = await this.openai.createTranscription(
+        createReadStream(filepath),
+        "whisper-1"
+      );
+      return response.data.text;
+    } catch (error) {
+      console.log("Error transcription", error.message);
+    }
+  }
 }
 
 export const openai = new OpenAI();
